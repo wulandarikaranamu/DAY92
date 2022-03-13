e85da6fb9634a99f8a449a4394ce753a6db25fae
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-03-14 00:06:22 +0700

    komentar

---
+++ percabangan.py
@@ -0,0 +1,23 @@
+# percabangan if
+
+data = 70
+if data < 80 :
+    print("nilai A")
+
+#percabangan if else
+ 
+nilai = 90
+if nilai > 80 :
+    print("selamat anda lulus")
+else:
+    print("maaf anda belum lulus")
+
+#percabangan elif 
+
+umur = 17
+if umur < 18 :
+    print("anda sudah layak bekerja")
+elif umur > 15 :
+    print("anda belum layak bekerja")
+else:
+    print("umur yang dinginkan tidak ada yang sesuai")
